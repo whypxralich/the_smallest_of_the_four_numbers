cdd7c36daefc07eb6e1964026a68e4a50733c458
whypxralich <whypxrlch@gmail.com>
2023-12-14 23:16:45 +0300

    Create stepik_4.1_the_smallest_of_the_four_numbers.py

---
+++ stepik_4.1_the_smallest_of_the_four_numbers.py
@@ -0,0 +1,17 @@
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+num4 = int(input())
+
+if num1>num2:
+    num1 = num2
+
+if num3>num4:
+    num3 = num4
+
+if num1>num3:
+    num1 = num3
+
+print (num1)
+
+
